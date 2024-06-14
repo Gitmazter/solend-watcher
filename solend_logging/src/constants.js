e10dcdf2baa6ea810171477d5f19841758f99e49
gitmazter <philip.andelic@gmail.com>
2024-06-14 14:13:25 +0800

    add liquidation action log

--- solend_logging/src/constants.js
+++ solend_logging/src/constants.js
@@ -1,6 +1,6 @@
 const action_logs =  {
-    'Program log: Instruction: InitializeAccount3':'create account',
-    'liquidate':'liquidate position',
+    'Program log: Create':'create account',
+    'Program log: Instruction: Liquidate Obligation and Redeem Reserve Collateral':'liquidate position',
     'Program log: Instruction: RedeemFees':'redeem fees',
     'Program log: Instruction: Withdraw Obligation Collateral and Redeem Reserve Collateral': "withdraw",
     'Program log: Instruction: Deposit Reserve Liquidity and Obligation Collateral': "deposit",
